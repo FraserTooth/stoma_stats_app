821ea3c4c68efea19b26577362e42da037dcdbf8
FraserTooth <frasertooth@gmail.com>
2021-01-26 00:29:02 +0800

    feat: initial commit

---
+++ lib/src/views/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime _previousChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: body());
+  }
+
+  String getDateString(DateTime date) {
+    final DateFormat formatter = DateFormat('dd-MM-yyyy HH:mm');
+    final String formatted = formatter.format(date);
+    return formatted;
+  }
+
+  void updateChangeDate(){
+    setState(() {
+          _previousChange=new DateTime.now();
+        });
+  }
+
+  Widget body() {
+    final displayString = _previousChange != null
+        ? getDateString(_previousChange)
+        : "No Previous Date Listed";
+
+    return Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(displayString),
+            ElevatedButton(
+              child: Text("Changed Stoma"),
+              onPressed: updateChangeDate,
+              )
+          ],
+        )
+    );
+  }
+}
